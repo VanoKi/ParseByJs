f6a8526e6a50a11bb6641f737dc55d22a5b571d6
VanoKi <kirsvano@gmail.com>
2024-12-23 22:38:14 +0300

    feat: check the true link

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ const fs = require('fs')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 
+const pagesNumber = 50
+const baseLink = `https://books.toscrape.com/catalogue/page-`
+let page = 1
+let parsingTimeout = 0
+
+function paginator() {
+    function getArticles() {
+        let link = `${baseLink}${page}.html`
+        return link
+    }
+    return getArticles()
+}
+
+console.log(paginator())
