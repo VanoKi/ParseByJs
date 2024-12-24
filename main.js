960a941a3346fb85e74b102303302385f0ed9c53
VanoKi <kirsvano@gmail.com>
2024-12-24 22:12:00 +0300

    feat: rewrite from function to global

--- main.js
+++ main.js
@@ -8,21 +8,6 @@ const baseLink = `https://books.toscrape.com/catalogue/page-`
 let page = 1
 let parsingTimeout = 0
 
-function paginator() {
-    function getArticles() {
-        let link = `${baseLink}${page}.html`
-        console.log(`Запрос по ссылке:${link}`)
-        axios.get(link)
-            .then(response => {
-                let currPage = response.data
-                const dom = new JSDOM(currPage)
-                let linksLength = dom.window.document.querySelectorAll('h3').length
-                for (let i = 0; i < linksLength; i++) {
-                    // let relLink = dom.window.document.querySelector('a')
-                    console.log(linksLength[i])
-                }
-            })
-    }
-}
-
-console.log(paginator())
+let link = `${baseLink}${page}.html`
+// const fetch = require('node-fetch')
+fetch(link).then(response => response.text()).then(data => console.log(data))
