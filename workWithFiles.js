5fbf7e23e219ba4da05ec5c4c82f415914c95c4d
VanoKi <kirsvano@gmail.com>
2025-01-08 13:34:38 +0300

    feat: returned to stat

--- workWithFiles.js
+++ workWithFiles.js
@@ -4,14 +4,18 @@ const path = require('path')
 
 const dirPath = 'C:\\Users\\VanoHa\\Downloads'
 // ic(path.resolve())
-// fs.stat(dirPath, (err, stats) => {
-//     if(err) {
-//         ic(err)
-//     } else {
-//        ic(stats)
+fs.stat(dirPath, (err, stats) => {
+    if(err) {
+        ic(err)
+    } else {
+       ic(stats)
+    }
+})
+// fs.readdir(dirPath, (err, items) => {
+//     if (err) ic(err)
+//     else {
+//         for (let i = 0; i < items.length; i++) {
+//             ic(items[i])
+//         }
 //     }
 // })
-fs.readdir(dirPath, (err, items) => {
-    if (err) ic(err)
-    else ic(items.length)
-})
