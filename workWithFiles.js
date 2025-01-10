2822d8498fdc0b58b242333e012d2e88e52ef7d8
VanoKi <kirsvano@gmail.com>
2025-01-10 12:34:17 +0300

    feat: work with Documents dir

--- workWithFiles.js
+++ workWithFiles.js
@@ -2,20 +2,11 @@ const ic = require('node-icecream')()
 const fs = require('fs')
 const path = require('path')
 
-const dirPath = 'C:\\Users\\VanoHa\\Downloads'
+const dirPath = 'C:\\Users\\VanoHa\\Documents\\'
 // ic(path.resolve())
-fs.stat(dirPath, (err, stats) => {
-    if(err) {
-        ic(err)
-    } else {
-       ic(stats)
-    }
+fs.readdir(dirPath, (err, items) => {
+    if (err) ic(err)
+    else {
+        items.forEach(item => ic(typeof item, item))
+        }
 })
-// fs.readdir(dirPath, (err, items) => {
-//     if (err) ic(err)
-//     else {
-//         for (let i = 0; i < items.length; i++) {
-//             ic(items[i])
-//         }
-//     }
-// })
