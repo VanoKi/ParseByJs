4693a6a2fa5df6c86c4b58cb82233aa43e4584f2
VanoKi <kirsvano@gmail.com>
2025-01-10 21:43:48 +0300

    feat: create a new file

--- readFile.js
+++ readFile.js
@@ -2,15 +2,24 @@ const fs = require('fs')
 const path = require('path')
 
 const filePath = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Modules in JavaScript.md'
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) console.log(err)
-    // console.log(data)
-    const lines = data.split('\n')
-    lines.forEach((line, index) => {
-        // console.log(`${index} ${line}`)
-        if (line === '') {
-            console.log(`${index}`)
-        }
+const outPath = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Modules in JavaScript mod.md'
+
+fs.writeFile(outPath, '', 'utf-8', (err) => {
+    if (err) {
+        console.error(err)
+    }
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) console.log(err)
+        // console.log(data)
+        const lines = data.split('\n')
+        lines.forEach((line, index) => {
+            // console.log(`${index} ${line}`)
+            if (line.trim() !== '') {
+                fs.appendFile(outPath, `${line}\n`, 'utf-8', (err) => {
+                    if (err) console.error(err)
+                    // else console.log('file created')
+                })
+            }
+        })
     })
-    // console.log(lines[12])
 })
