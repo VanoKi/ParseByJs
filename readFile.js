fca34f714089807bfb45d74653294c2c4f8567a8
VanoKi <kirsvano@gmail.com>
2025-01-10 21:16:40 +0300

    feat: split on the lines

--- readFile.js
+++ readFile.js
@@ -4,7 +4,8 @@ const path = require('path')
 const filePath = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Modules in JavaScript.md'
 fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) console.log(err)
-    else {
-        console.log(data)
-    }
+    const lines = data.split('\n')
+    lines.forEach((line, index) => {
+        console.log(`${index} ${line}`)
+    })
 })
