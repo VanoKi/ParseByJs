e9fbd05b58668ca05a17635f84ecd78dee0c3921
VanoKi <kirsvano@gmail.com>
2025-01-10 21:23:29 +0300

    feat: find empty strings in the file

--- readFile.js
+++ readFile.js
@@ -4,8 +4,13 @@ const path = require('path')
 const filePath = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Modules in JavaScript.md'
 fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) console.log(err)
+    // console.log(data)
     const lines = data.split('\n')
     lines.forEach((line, index) => {
-        console.log(`${index} ${line}`)
+        // console.log(`${index} ${line}`)
+        if (line === '') {
+            console.log(`${index}`)
+        }
     })
+    // console.log(lines[12])
 })
