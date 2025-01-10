057ff515816c32d474092def1b414ea4d8b920ff
VanoKi <kirsvano@gmail.com>
2025-01-10 18:26:50 +0300

    use then to work funcruin

--- workWithFiles.js
+++ workWithFiles.js
@@ -4,14 +4,16 @@ const path = require('path')
 
 const dirPath = 'C:\\Users\\VanoHa\\Documents\\'
 // ic(path.resolve())
-let obdisianDir
-fs.readdir(dirPath, (err, items) => {
-    if (err) ic(err)
-    else {
-        obdisianDir = items.filter(item => item.match(/obsidian/gi))
-        ic(obdisianDir)
-    }
+async function findObsidianDir() {
+    const items = await fs.promises.readdir(dirPath)
+    const obsidianDir = items.filter(item => item.match(/obsidian/ig))
+    return obsidianDir
+}
+
+// let obsDir = findObsidianDir()
+// console.log(obsDir)
+findObsidianDir().then(result => {
+    console.log(result)
+}).catch(err => {
+    console.log(err)
 })
-// ic(obdisianDir)
-// const obdisianDirPath = path.join(dirPath, obdisianDir)
-// ic(obdisianDirPath)
