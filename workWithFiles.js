4e6a6e8a1a832d3fb2ca2952f52e12912b6683a1
VanoKi <kirsvano@gmail.com>
2025-01-10 13:40:44 +0300

    feat: add callback to code

--- workWithFiles.js
+++ workWithFiles.js
@@ -4,9 +4,14 @@ const path = require('path')
 
 const dirPath = 'C:\\Users\\VanoHa\\Documents\\'
 // ic(path.resolve())
+let obdisianDir
 fs.readdir(dirPath, (err, items) => {
     if (err) ic(err)
     else {
-        items.forEach(item => ic(typeof item, item))
-        }
+        obdisianDir = items.filter(item => item.match(/obsidian/gi))
+        ic(obdisianDir)
+    }
 })
+// ic(obdisianDir)
+// const obdisianDirPath = path.join(dirPath, obdisianDir)
+// ic(obdisianDirPath)
