644c5d0d94560a364eb4f26b7382cf3591d02033
VanoKi <kirsvano@gmail.com>
2025-01-12 22:56:39 +0300

    feat: add reading two files

--- compareFiles.js
+++ compareFiles.js
@@ -4,3 +4,12 @@ const path = require('path')
 const file1 = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Introduction to Bundlers mod.md'
 const file2 = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\reply from GPT Bundlers.md'
 
+fs._readFile(file1, 'utf-8', (err, data1) => {
+    if (err) console.error(err)
+    fs._readFile(file2, 'utf-8'. (err, data2) => {
+        if (err) console.error(err)
+        const lines1 = data1.split('\n')
+        const lines2 = data2.split('\n')
+
+    })
+})
