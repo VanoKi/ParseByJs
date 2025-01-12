f2a5c25d6833dcd9e5dc329ead4c5bac8db7f075
VanoKi <kirsvano@gmail.com>
2025-01-13 00:49:01 +0300

    fix: try to split by number

--- compareFiles.js
+++ compareFiles.js
@@ -8,9 +8,9 @@ fs.readFile(file1, 'utf-8', (err, data1) => {
     if (err) console.error(err)
     fs.readFile(file2, 'utf-8', (err, data2) => {
         if (err) console.error(err)
-        const lines1 = data1.split('\n')
+        const lines1 = data1.split(/(?=\d+\.\s)/).map(i => i.split('\n'))
         const lines2 = data2.split('- ').map(i => i.trim())
-        // console.log(lines1)
-        console.log(lines2)
+        console.log(lines1)
+        // console.log(lines2)
     })
 })
