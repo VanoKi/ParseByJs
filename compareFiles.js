a00d0c9e43a5c8e137c952f5f65a704e08851eef
VanoKi <kirsvano@gmail.com>
2025-01-13 01:14:33 +0300

    fix: add header

--- compareFiles.js
+++ compareFiles.js
@@ -15,13 +15,18 @@ fs.writeFile(outPath, '', 'utf-8', (e) => {
             const lines2 = data2.split('- ').map(i => i.trim())
             for (let i = 0; i < lines1.length; i++) {
                 for (let j = 0; j < lines1[i].length; j++) {
+                    if (j == 0) {
+                        fs.appendFile(outPath, `##### ${lines1[i][j]}\n`, 'utf-8', (err) => {
+                            if (err) console.error(err)
+                        })
+                    }
                     if (lines1[i][j] === lines2[i]) {
-                        // console.log(`**${lines1[i][j]}**`)
+                        console.log(`**${lines1[i][j]}**`)
                         fs.appendFile(outPath, `**${lines1[i][j]}**\n`, 'utf-8', (err) => {
                             if (err) console.error(err)
                         })
                     } else {
-                        // console.log(lines1[i][j])
+                        console.log(lines1[i][j])
                         fs.appendFile(outPath, `${lines1[i][j]}\n`, 'utf-8', (err) => {
                             if (err) console.error(err)
                         })
