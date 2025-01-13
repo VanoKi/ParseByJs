141c5e365acbbcae0de0e60fd1d88d75dba626ef
VanoKi <kirsvano@gmail.com>
2025-01-13 12:25:26 +0300

    feat: try to compare another one file

--- compareFiles.js
+++ compareFiles.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 
-const file1 = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Introduction to Bundlers mod.md'
-const file2 = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\reply from GPT Bundlers.md'
-const outPath = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Introduction to Bundlers mod2.md'
+const file1 = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Modules in JavaScript mod.md'
+const file2 = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\reply from GPT.md'
+const outPath = 'C:\\Users\\VanoHa\\Documents\\Obsidian Vault\\St1 Modules in JavaScript mod2.md'
 
 fs.writeFile(outPath, '', 'utf-8', (e) => {
     if (e) console.error(e)
@@ -20,7 +20,7 @@ fs.writeFile(outPath, '', 'utf-8', (e) => {
                 for (let j = 1; j < lines1[i].length; j++) {
                     if (lines1[i][j] === lines2[i]) {
                         // console.log(`**${lines1[i][j]}**`)
-                        fs.appendFile(outPath, `**${lines1[i][j]}**\n`, 'utf-8', (err) => {
+                        fs.appendFile(outPath, `__${lines1[i][j]}__\n`, 'utf-8', (err) => {
                             if (err) console.error(err)
                         })
                     } else {
