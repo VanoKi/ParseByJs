0604ec3682ddd02fd5891d07e98f0b60a3410503
VanoKi <kirsvano@gmail.com>
2025-02-10 19:44:50 +0300

    fix: count

--- headFirst/script.js
+++ headFirst/script.js
@@ -56,9 +56,11 @@
 // }
 
 class Car {
+    static count = 0;
     constructor(model, year) {
         this.model = model;
         this.year = year;
+        Car.count++;
     }
     getInfo() {
         return `This is a ${this.model} car from ${this.year}`;
@@ -68,27 +70,46 @@ class Car {
         const currentYear = new Date().getFullYear();
         return currentYear - this.year;
     }
+    static countCars() {
+        return Car.count;
+    }
 }
+
 
 
 const myCar = new Car("Toyota", 2020);
 
-// console.log(myCar.getInfo());
-// console.log(myCar.age());
-
 
 class ElectricCar extends Car {
+    #battery;
     constructor(model, year, battery) {
+
         super(model, year);
-        this.battery = battery;
+        this.#battery = battery;
+    }
+    getInfo() {
+        return `${super.getInfo()} with a ${this.#battery} kWh battery`;
     }
 
-    getBatteryInfo() {
-        return `${super.getInfo()} with a ${this.battery} kWh battery`;
+
+    get battery() {
+        return this.#battery;
     }
+    set battery(value) {
+        if (value > 0) {
+            this.#battery = value;
+        } else {
+            console.error("Battery must be greater than 0");
+        }
+    }
+
+
+    getBatteryInfo() {
+        return `${super.getInfo()} with a ${this.#battery} kWh battery`;
+    }
+
+
 }
 const myElectricCar = new ElectricCar("Tesla", 2022, 100);
 console.log(myElectricCar.getInfo());
-console.log(myElectricCar.getBatteryInfo());
-console.log(myElectricCar.age());
 
