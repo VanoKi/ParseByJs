8e3f4da7257c6e515bc6bd994ff9aa2308468fad
VanoKi <kirsvano@gmail.com>
2025-08-12 10:21:23 +0300

    feat: parse use cheerio

--- parse.js
+++ parse.js
@@ -5,10 +5,24 @@ const cheerio = require('cheerio')
 async function getData() {
     try {
         const response = await axios.get(' https://quotes.toscrape.com/')
-        console.log(response)
+        // console.log(response.data)
+        // return response
+        const html = response.data
+
+        const $ = cheerio.load(html)
+
+        $('.quote').each((i, elem) => {
+            const text = $(elem).find('text').text()
+            const author = $(elem).find('.author').text()
+            const tags = $(elem).find('.tag').map((i, e) => $(e).text()).get()
+
+            console.log(`${i + 1}. ${text} - ${author}`)
+            console.log(`Tags - ${tags,join(', ')}`)
+            console.log(`----`)
+        })
     } catch (error) {
         console.error(error)
     }
 }
 
-getData()
+
